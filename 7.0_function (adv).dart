f8f2efb96052887aa76de616ad611381e5ce9816
Hasan Ahmad <80910782+HasanAhmad9812021@users.noreply.github.com>
2022-12-22 12:10:52 +0600

    Add files via upload

---
+++ 7.0_function (adv).dart
@@ -0,0 +1,20 @@
+// It is standard way for function for dart language
+// Cause in the flutter this type of function is most used
+
+void main() {
+  addTwoNumbers(firstNumber: 5, secondNumber: 5);
+  displayInformation("Hasan Ahmad", 21, "Software Engineer");
+}
+
+void addTwoNumbers({required int firstNumber, int secondNumber = 0}) {
+  double sum = (firstNumber + secondNumber) * 12 / 12;
+  print(sum);
+}
+
+void displayInformation(String name, int age, String profession,
+    [String phone = 'Unknown'] /*--> this is optional parameter */) {
+  print(name);
+  print('Age : $age');
+  print("Profession : $profession");
+  print("Cell : $phone"); // for optional parameter
+}
