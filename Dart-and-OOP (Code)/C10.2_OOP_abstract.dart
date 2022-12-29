194c6ab00306f799594eeb312dcae63920ede08c
Hasan Ahmad <80910782+HasanAhmad9812021@users.noreply.github.com>
2022-12-29 17:18:01 +0600

    updated

--- Dart-and-OOP (Code)/C10.2_OOP_abstract.dart
+++ Dart-and-OOP (Code)/C10.2_OOP_abstract.dart
@@ -1,4 +1,6 @@
 // abstract
+// object is not applicable for abstract class
+// abstract class inherit using 'implements' keyword
 
 abstract class letter {
   void write();
@@ -9,6 +11,10 @@ class hasanLetter implements letter {
   void write() {
     print('I wanna goto canada');
   }
+
+  void display() {
+    print('Hasan letter done');
+  }
 }
 
 class rahatLetter implements letter {
@@ -16,12 +22,28 @@ class rahatLetter implements letter {
   void write() {
     print('I wanna goto germany');
   }
+
+  void display() {
+    print('Rahat letter done');
+  }
 }
 
 void main() {
-  hasanLetter ob1 = new hasanLetter();
-  ob1.write();
+  // no object support for abstract class
 
-  rahatLetter ob2 = new rahatLetter();
-  ob2.write();
+  hasanLetter ob = new hasanLetter();
+  ob.write();
+  ob.display();
+
+  rahatLetter ob1 = new rahatLetter();
+  ob1.write();
+  ob1.display();
+
+  // // using polymorphism
+  // letter ob2 = new hasanLetter();
+  // ob2.write();
+  // ob2.display(); // can't call by this cause of polymorphism
+
+  // ob2 = new rahatLetter();
+  // ob2.write();
 }
--- Dart-and-OOP (Code)/C10.3_polymorphism.dart
+++ Dart-and-OOP (Code)/C10.3_polymorphism.dart
@@ -1,27 +1,62 @@
 // polymorphism
+// abstract class inherit using 'extends' keyword
 
 abstract class letter {
   void write();
+
+  void display() {
+    print('Hello world');
+  }
 }
 
-class hasanLetter implements letter {
+class hasanLetter extends letter {
+  // inherit the display(); method from super class
   @override
   void write() {
     print('I wanna goto canada');
   }
+
+  void printInfo() {
+    super.display();
+    print('Hasan subclass');
+  }
 }
 
-class rahatLetter implements letter {
+class rahatLetter extends letter {
+  // inherit the display(); method from super class
   @override
   void write() {
     print('I wanna goto germany');
+  }
+
+  void printInfo() {
+    super.display();
+  }
+}
+
+class sammunLetter extends letter {
+  // inherit the display(); method from super class
+  @override
+  void write() {
+    print('I wanna goto USA');
+  }
+
+  void printInfo() {
+    super.display();
   }
 }
 
 void main() {
   letter ob1 = new hasanLetter();
   ob1.write();
+  ob1.display();
 
   ob1 = new rahatLetter();
   ob1.write();
+  ob1.display();
+  // ob2.printInfo(); // can't call because polymorphism
+
+  ob1 = new sammunLetter();
+  ob1.write();
+  // ob1.printInfo();  // can't call because polymorphism
 }
